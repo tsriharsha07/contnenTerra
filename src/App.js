cdc87ad50c0159892269e88f48d2501b5f9e60eb
tsriharsha07 <tsriharsha07@gmail.com>
2022-12-29 11:20:40 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/reactjs.json').then(
+      response => response.json()
+    ).then(json => setData(json))
+  }, [])
+
+  console.log(data && data.data && data.data.children)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {data && data.data.children && data.data.children.map((item, index) => <div key={index} className="mainClass">
+        <div class="card body1">
+          <div class="card-header">
+            {item.data.title}
+          </div>
+          <div class="card-body">
+          <p class="card-text">{item.data.selftext_html}</p>
+            <h5 class="card-title">{item.data.score}</h5>
+            
+            <a href={item.data.url} class="btn btn-primary">Go to the given link</a>
+          </div>
+        </div>
+        
+      </div>)}
+
     </div>
   );
 }
